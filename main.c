cbc59b0202626791daa90f50da7d54d186e10f9c
TechWithEd <97378772+TechWithEd@users.noreply.github.com>
2023-05-10 22:09:53 +0630

    Update main.c

--- main.c
+++ main.c
@@ -5,13 +5,13 @@
 int main(void)
 {
     hashtable_t table = hashtable_new(10);
-    hashtable_insert(table, "hyunjin", 100);
-    hashtable_insert(table, "felix", 95);
-    hashtable_insert(table, "lee know", 75);
+    hashtable_insert(table, "john", 27);
+    hashtable_insert(table, "steve", 43);
+    hashtable_insert(table, "michael", 18);
 
-    printf("%d\n", *hashtable_get(table, "hyunjin"));
-    printf("%d\n", *hashtable_get(table, "felix"));
-    printf("%d\n", *hashtable_get(table, "lee know"));
+    printf("%d\n", *hashtable_get(table, "john"));
+    printf("%d\n", *hashtable_get(table, "steve"));
+    printf("%d\n", *hashtable_get(table, "michael"));
     hashtable_free(table);
     return 0;
 }
